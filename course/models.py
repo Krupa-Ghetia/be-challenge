e69ec29f441a106adb0579b314eb0977d08c0efd
Krupa_Ghetia <krupa.ghetia@increff.com>
2021-10-31 14:14:44 +0530

    Create course model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from users.models import User
+from subjects.models import Subjects
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subjects)
+    row_created = models.DateTimeField(default=datetime.now)
+    row_last_updated = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
