4211b253d2253b68cfadb2a65d07bcbb1f77be71
Krupa_Ghetia <krupa.ghetia@increff.com>
2021-11-01 05:24:28 +0530

    Create lessons model

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from users.models import User
+from course.models import Course
+
+
+class Lessons(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    courses = models.ManyToManyField(Course)
+    row_created = models.DateTimeField(default=datetime.now)
+    row_last_updated = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = 'Lesson'
+        verbose_name_plural = 'Lessons'
