a78dfab3d7210942e3bf0daba95884455e1963df
Krupa_Ghetia <krupa.ghetia@increff.com>
2021-11-01 18:51:21 +0530

    Create Videos model.

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from users.models import User
+from lessons.models import Lessons
+from tags.models import Tags
+
+
+class Videos(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    link = models.URLField(null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags)
+    lessons = models.ManyToManyField(Lessons)
+    row_created = models.DateTimeField(default=datetime.now)
+    row_last_updated = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
